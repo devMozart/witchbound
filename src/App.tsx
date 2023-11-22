bccbf9e7b4dc269a80e7fed0db3b99a3cb4ebb60
Amadeus Hein <amadeus.hein@ica.se>
2023-11-22 08:39:02 +0100

    Create a starter React project with Vite and ChakraUI

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Image } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <Image borderRadius="xl" w="40vw" src="./images/witchbound.webp" />
+    </>
+  );
+}
+
+export default App;
